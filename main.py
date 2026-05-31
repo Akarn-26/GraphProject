8778ba2e2c9994eb59b1e4c6edb3c394eeb5cc1f
Akarn <akkarn26@gmail.com>
2026-05-31 16:50:17 +0545

    fix

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from contextlib import asynccontextmanager
 from neo4j import GraphDatabase
 import os
 from dotenv import load_dotenv
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+import traceback
 load_dotenv()
 
 @asynccontextmanager
@@ -17,11 +20,18 @@ async def lifespan(app:FastAPI):
     print("DB loaded")
     yield
     app.state.driver.close()
-
 app=FastAPI(lifespan=lifespan)
+@app.exception_handler(Exception)
+async def global_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=500,
+        content={"error": str(exc), "trace": traceback.format_exc()}
+    )
 class UserQuery(BaseModel):
     query:str
-
+@app.get("/")
+def home():
+    return {"Message":"Welcome to Neo4j graph Project"}
 @app.get("/health")
 def health(request:Request):
     ready=getattr(request.app.state,"ready",False)
